7fb12389dbc7702acbde36bf1076b9901d30ab36
qingl <412753863@qq.com>
2022-04-07 21:04:37 +0800

    重新开始项目

--- lcov/server.sh
+++
@@ -1,3 +0,0 @@
-# sudo npm install live-server -g
-
-live-server --port=5500 lcov/html
---
+++ microlife/hello.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+// hello world
+TEST(Hello, World) {
+    std::cout << "Hello, World!" << std::endl;
+}
